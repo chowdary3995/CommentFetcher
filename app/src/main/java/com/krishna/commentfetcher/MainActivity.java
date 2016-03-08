36a262a761f334198548ccd98d50d07816cacb42
chowdary3995 <krishnamohan.sajja@gmail.com>
2016-03-08 13:29:50 -0600

    commit local change three

--- app/src/main/java/com/krishna/commentfetcher/MainActivity.java
+++ app/src/main/java/com/krishna/commentfetcher/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //local change one
         setContentView(R.layout.activity_main);
 
         authorList = new ArrayList<String>();
